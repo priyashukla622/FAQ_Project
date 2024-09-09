4757d7d4ef2325c52978f74a9d92a26dfb98f8ec
priya shukla <priyashukla22@navgurukul.org>
2024-09-09 12:22:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+
+const accordians = document.querySelectorAll(".accordian");
+accordians.forEach(accordian => {
+document.body.addEventListener("click", (event) => {
+    const accordion = event.target.closest(".accordian");
+
+    if (accordion) {
+        const icon = accordion.querySelector(".icon");
+        const answer = accordion.querySelector(".answer");
+
+        icon.classList.toggle("active");
+
+        if (icon.classList.contains("active")) {
+            answer.style.maxHeight = null;
+        } else {
+            answer.style.maxHeight = answer.scrollHeight + "px";
+        }
+    }
+});
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
